d3b42ae2cbaa72a3f1b0b03331a14a832ffb4159
ValentineFlo <vflorens@gaming.tech>
2025-01-24 15:33:09 +0100

    console

--- api/include/Console.h
+++ api/include/Console.h
@@ -1,1 +1,37 @@
 #pragma once
+#include <vector>
+#include <iostream>
+#include "windows.h"
+
+namespace Mediatheque
+{
+
+    class Console
+    {
+    private:
+        static std::vector<std::string> screenBuffer;
+
+    public:
+
+        static std::string read()
+        {
+            std::string input;
+
+            HANDLE hConsole = GetStdHandle(STD_INPUT_HANDLE);
+            char buffer[1024];
+            DWORD charsRead;
+            ReadConsoleA(hConsole, buffer, sizeof(buffer), &charsRead, nullptr);
+            if (charsRead > 0)
+            {
+                if (buffer[charsRead - 1] == '\n') charsRead--;
+                if (buffer[charsRead - 1] == '\r') charsRead--;
+                input.assign(buffer, charsRead);
+            }
+            return input; 
+        }
+
+    };
+
+    std::vector<std::string> Console::screenBuffer;
+}
+
--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -1,66 +1,37 @@
 #include <iostream>
-#include "Api.h"
-#include "process.h"
+#include "Process.h"
+#include "Console.h"
 
 
 int main()
 {
-	    Process proces;
+    
+    Process process;
+    std::cout << "Bienvenue dans la mediatheque!" << std::endl;
+    std::cout << "exit pour quitter" << std::endl;
+    std::cout << "--help pour l'aide" << std::endl;
 
-    std::vector<std::string> instructionList =
+    while (true)
     {
-         "addClient Jean Dupont 25 rue 123456789 jean@email.com"
-        ,"addClient charlotte Dupont 25 rue 123456789 jean@email.com"
-        ,"addMedia Book dfdfi 265"
-        ,"addMedia Movie jdjif dfdfi 18"
-        ,"rent Dupont Book dfdfi"
-        ,"stateMedia Book dfdfi"
-        ,"listMedia"
-        ,"listClient"
-        ,"removeClient jean@email.com"
-        ,"who Book dfdfi"
-        ,"who Movie jdjif"
-        ,"returnMedia Book dfdfi"
-        ,"who Book dfdfi"
-        ,"showMedia Jean Dupont"
-    };
 
+        std::string commandline = Mediatheque::Console::read();
 
-
-    while (!instructionList.empty())
-    {
-        std::string instruction = instructionList.front();
-        instructionList.erase(instructionList.begin());
-
-
-        try
+        if (commandline == "exit")
         {
-           proces.process(instruction);
-        }
-        catch (std::runtime_error& e)
-        {
-            std::cerr << e.what();
+            break;
         }
 
-        ::getchar();
+        if (!commandline.empty())
+        {
+            try
+            {
+                process.process(commandline);
+            }
+            catch (const std::runtime_error& e)
+            {
+                std::cout << "Erreur: " + std::string(e.what()) << std::endl;
+            }
+        }
+
     }
-
-    return 0;
-	std::string msg;
-	void* data = new std::string("hello");
-
-	getInterpreter().addCommand("echo", msg, data);
-	msg += "toto";
-	getInterpreter().addCommand("echo", msg, data);
-
-	cmd_init();
-	cmd_tick();
-	std::cout << msg << std::endl;
-	cmd_tick();
-	std::cout << msg << std::endl;
-	
-
-	cmd_shutdown();
-	
-	return 0;
 }
